075caa1932534d71680d21a6aebf39fa45b17845
Reinis Ikass <reinis.ikass@gmail.com>
2023-11-22 16:32:34 +0200

    feat: update gems, add Shoelace

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,5 +1,17 @@
 import "$styles/index.css";
 
+// Example Shoelace components. Mix 'n' match however you like!
+import '@shoelace-style/shoelace/dist/components/breadcrumb/breadcrumb.js';
+import '@shoelace-style/shoelace/dist/components/breadcrumb-item/breadcrumb-item.js';
+import "@shoelace-style/shoelace/dist/components/button/button.js"
+import '@shoelace-style/shoelace/dist/components/card/card.js';
+import "@shoelace-style/shoelace/dist/components/icon/icon.js"
+import '@shoelace-style/shoelace/dist/components/icon-button/icon-button.js';
+
+// Use the public icons folder:
+import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js"
+setBasePath("/shoelace-assets")
+
 // Import all JavaScript & CSS files from src/_components
 import components from "$components/**/*.{js,jsx,js.rb,css}";
 import "./external_links";
