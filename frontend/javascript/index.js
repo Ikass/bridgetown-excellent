fb1bbc7900d15f596fab95c1a1a0d81fec8cce76
Reinis Ikass <reinis.ikass@gmail.com>
2024-01-17 16:03:28 +0200

    add Turbo

--- frontend/javascript/index.js
+++ frontend/javascript/index.js
@@ -1,4 +1,18 @@
 import "$styles/index.css";
+import * as Turbo from "@hotwired/turbo"
+
+/**
+ * Adds support for declarative shadow DOM. Requires your HTML <head> to include:
+ * `<meta name="turbo-cache-control" content="no-cache" />`
+ */
+import * as TurboShadow from "turbo-shadow"
+
+/**
+ * Uncomment the line below to add transition animations when Turbo navigates.
+ * Use data-turbo-transition="false" on your <main> element for pages where
+ * you don't want any transition animation.
+ */
+import "./turbo_transitions.js"
 
 // Example Shoelace components. Mix 'n' match however you like!
 import '@shoelace-style/shoelace/dist/components/breadcrumb/breadcrumb.js';
@@ -15,7 +29,6 @@ setBasePath("/shoelace-assets")
 // Import all JavaScript & CSS files from src/_components
 import components from "$components/**/*.{js,jsx,js.rb,css}";
 import "./external_links";
-// import "./menu_toggle.js";
 import "@justinribeiro/lite-youtube";
 
 console.info("Bridgetown is loaded!");
